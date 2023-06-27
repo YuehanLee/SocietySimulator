1df3373d965449a63eaa44ff751e988c146426a1
YuehanLee <li2803@purdue.edu>
2023-06-27 16:34:03 -0400

    Initial

---
+++ Source/SocietySimulator/SocietySimulatorGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SocietySimulatorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOCIETYSIMULATOR_API ASocietySimulatorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
